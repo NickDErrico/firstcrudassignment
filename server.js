4e842448ad748ce6771afb7e39dbdef40fb648a5
NickDErrico <nickderrico@hotmail.com>
2018-01-08 16:00:21 -0700

    refactored via Troy

--- server.js
+++ server.js
@@ -1,63 +1,60 @@
 let express = require('express');
 let fs = require('fs');
 let app = express();
-let port = process.env.PORT || 8000;
+let bodyParser = require('body-parser');
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.json())
+
+app.post('/users', function(req, res) {
+  let data = fs.readFileSync('./storage.json', 'utf-8');
+  let parsedData = JSON.parse(data);
+  parsedData.push(req.body);
+  fs.writeFileSync('./storage.json', JSON.stringify(parsedData));
+  res.send('user created')
+});
 
 app.get('/users', function(req, res) {
-  fs.readFile('./storage.json', 'utf-8', function(err, data) {
-    res.json(JSON.parse(data));
-  })
+  let data = fs.readFileSync('./storage.json', 'utf-8');
+  let parsedData = JSON.parse(data);
+  res.json(parsedData);
 });
 
 app.get('/users/:name', function(req, res) {
-  fs.readFile('./storage.json', 'utf-8', function(err, data) {
-    let parsedData = JSON.parse(data);
-    let matchedUser = parsedData.filter((item) => {
-      return item.name == req.params.name;
-    });
-    if (matchedUser.length >= 1) {
-      res.json(matchedUser)[0];
-    } else {
-      res.send('user not found')
-    }
+  let data = fs.readFileSync('./storage.json', 'utf-8');
+  let parsedData = JSON.parse(data);
+  let matchedUser = parsedData.filter((item) => {
+    return item.name === req.params.name;
   })
-});
+  res.json(matchedUser[0])
+})
 
-app.put('/modify/:name/:newname', function(req, res) {
-  fs.readFile('./storage.json', 'utf-8', function(err, data) {
-    let parsedData = JSON.parse(data);
-    let matchedUser = parsedData.filter((item) => {
-      return item.name == req.params.name;
-    });
-    if (matchedUser.length >= 1) {
-      matchedUser[0].name = req.params.newname;
-      res.json(matchedUser)[0]
+app.patch('/users/:name', function(req, res) {
+  let data = fs.readFileSync('./storage.json', 'utf-8');
+  let parsedData = JSON.parse(data);
+  let updatedData = parsedData.map((item) => {
+    if (item.name === req.params.name) {
+      return req.body;
     } else {
-      res.send('user does not exist')
+      return item;
     }
   })
-});
+  fs.writeFileSync('./storage.json', JSON.stringify(updatedData));
+  res.send('User updated')
+})
+
 
 app.delete('/delete/:name', function(req, res) {
+  let data = fs.readFileSync('./storage.json', 'utf-8');
+  let parsedData = JSON.parse(data);
+  let filteredData = parsedData.filter((item) => {
+    return item.name !== req.params.name;
+  });
+  fs.writeFileSync('./storage.json', JSON.stringify(filteredData));
+  res.send('User deleted')
+})
 
-});
 
-app.post('/create/:name/:email/:state', function(req, res) {
-  let userInfo = {
-    name: req.params.name,
-    email: req.params.email,
-    state: req.params.state
-  };
-
-  fs.readFile('./storage.json', 'utf-8', function(err, data) {
-    let usersArr = JSON.parse(data);
-
-    usersArr.push(userInfo);
-    fs.writeFile('./storage.json', JSON.stringify(usersArr), function(err) {
-      res.send('user created')
-    });
-  })
-});
 
 app.listen(port, function() {
   console.log('Listening on port', port);
